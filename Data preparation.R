11a4054d2c23324353f626762ea222bff6d1c9ed
MartinLuquetEcology <58080958+MartinLuquetEcology@users.noreply.github.com>
2020-09-23 20:51:47 +0200

    Add files via upload

---
+++ Data preparation.R
@@ -0,0 +1,103 @@
+## This script will guide you step by step to do classification and/or prevalence estimation for feeding inference
+
+## It is complementary to the paper 
+## "Inferring insect feeding patterns based on sugar profiles:a comparison of statistical methods"
+## (Ecological Entomology)
+## Notably the heatmap decision tool displayed in the paper
+
+## For any question/suggestion, or if you notice a bug
+## Please contact Martin Luquet at martin.luquet.pro@gmail.com
+## Don't hesitate to give your feedback!
+
+#### To use this script, you will need the following packages
+## vegan
+## ggplot2 (only if you want nice graphs)
+## And one of three other packages according to the method you choose:
+## see later in the script
+
+## To install them, use the following functions
+# install.packages('vegan')
+# install.packages('ggplot2')
+
+## Loading the packages
+library(vegan)
+library(ggplot2)
+
+
+
+  #### LOADING THE DATA ####
+
+## Important notes:
+# This script will only help you to choose and use algorithms to predict your field data based on your lab data
+# The choice of the predictor variables should be done before 
+# Based on biological knowledge, but also data visualization, etc.
+# Your data should be prepared (see below) and visualized beforehand
+# Similarly, feeding classes must be set beforehand 
+# e.g. : will starved individuals considered to belong to the same class as unfed individuals, etc.
+
+## Data preparation:
+# The data must ideally be contained in a .txt file
+# Although it should work if you import it directly from Excel
+# The data must contain one row per individual
+# One column must contain the feeding classes to which insects are assigned 
+# Example: Nectar-fed, Unfed, Honeydew-fed
+# Other variables contain predictor variables, i.e. sugar variables used for prediction
+# You can use as many variables as you want
+# You're not restricted to sugar-related variables
+# Your dataset can also contain other variables, not used for prediction (e.g. sampling date, ...)
+
+
+# Before loading the dataset, you have to set the working directory
+# Session/Set Working Directory/Choose directory
+# You can now load your data using the following function
+# Replace 'fake_lab_data' by the name of your file
+# lab.data <- read.table(name='your_lab_data.txt',header=T)
+# Or keep it as such to run an example
+lab.data <- read.table('fake_lab_data.txt',header=T)
+
+# (Note: if you prefer to import the data from Excel, make sure to name it "lab.data")
+
+
+## You can view your dataset using this function
+# In the example:
+# The column "Sample_ID" is just an ID number for the samples
+# The column "Tmt" contains the feeding treatment (Nectar, Unfed, Honeydew)
+# Other columns contain the sugar predictive variables (here there are 4 of them)
+View(lab.data)
+
+## Now, we will just need some information about your variables
+
+# First, please indicate what column contains the feeding classes in your dataset (e.g. unfed, nectar-fed,...)
+# Change "Tmt" between quotation marks by the variable containing classes in your dataset
+Classes.var <- "Tmt"
+
+# Now run the following lines
+lab.data <- as.data.frame(lab.data)
+lab.data$Classes <- as.factor(lab.data[,Classes.var])
+Classes <- lab.data$Classes 
+
+# Now, please indicate what the predictors are
+# Between the brackets, indicate the column numbers containing the predictive variables
+# Separated by commas (e.g. here 3,4,5,6)
+  # IMPORTANT : There should be NO MISSING VALUES in these columns
+  # If there are some, remove the corresponding lines from the table
+Pred.col <- c(3,4,5,6)
+# Now run the following line
+Predictors <- lab.data[,Pred.col]
+
+
+  #### DEFINING NOISE INDEX ####
+
+## The Noise Index is computed using a Redundancy Analysis, as follows:
+RDA.an <- rda(Predictors,Classes)
+
+## The object noise.Ind gives your noise index
+## 0.64 in the example
+## Refer to the heatmap in the paper to choose a method according to this index!
+noise.Ind <- round(summary(RDA.an)$unconst.chi/summary(RDA.an)$tot.chi, 2)
+noise.Ind
+
+## You can now choose your classifier, using the heatmap
+  # Random Forest -> Open the script "Random Forest.R"
+  # Discriminant Analyses -> Open the script "Discriminant Analyses.R"
+  # Gaussian Mixture Models -> Open the script "Gaussian Mixture Models.R"
---
+++ Gaussian Mixture Models.R
@@ -0,0 +1,59 @@
+#### TRAINING THE CLASSIFIER (ON THE LAB DATA) ####
+
+## This script will help you predict insect feeding status using Gaussian Mixture Models
+## To run this script, you must have run first "Data preparation.R"
+## And chosen Gaussian Mixture Models as a method
+
+## Let's train a Gaussian Mixture Model classifier on the lab dataset (GMM)
+# To do so, we need the package mclust
+# If you don't have it, use the following function: install.packages('mclust')
+library(mclust)
+
+# We train the model using the variables you have already defined
+GMM.model <-  MclustDA(Predictors,Classes)
+
+# The model is fitted
+# Using the following function, you can see how your data are distributed among the different variables
+plot(GMM.model,what="classification")
+
+# You can then see how each class is modelled by the GMM along each variable
+plot(GMM.model,what="scatterplot")
+
+
+# The two following lines tell you how individuals in the lab data are predicted by the model
+lab.data$Predicted <- predict(GMM.model)$classification
+lab.data$Predicted 
+
+# We can use it to generate the CONFUSION MATRIX
+# It indicates how individuals were classified in the lab dataset
+# Rows are real individuals, columns are predictions
+conf.matrix <- table(Classes,predict(GMM.model)$classification)
+conf.matrix
+
+# Here are the estimated error rates associated to each class
+1 - diag(conf.matrix)/rowSums(conf.matrix)
+
+# This gives you the samples (line numbers) that were misclassified
+which(lab.data$Classes != lab.data$Predicted)
+
+# Don't hesitate to visualize your data to see what samples were correctly or wrongly classified
+
+# You can do it directly in R using the following function
+# Just change the parameters "y" and "x" in the following function with the variables you want
+# (Here: Fructose on the x-axis, GF_Ratio on the y axis)
+# The shape shows you the real class of each sample
+# The colour shows you the predicted class
+
+ggplot(lab.data,aes(x=Fructose,y=GF_Ratio,shape=Classes,col=Predicted))+
+  geom_point(size=2)+
+  labs(shape="Real Class", col="Predicted Class")
+
+# If you prefer to export the data to another software (e.g. Excel or Open Office)
+# you can export the dataset containing predicitons using the following function
+# This will create a text file named "lab.data.predictions.txt" in your working directory
+# This is your dataset, containing the predictions by the Random Forest
+# The column "Predicted" indicates how each individual is predicted by the Random Forest
+write.table(lab.data[,colnames(lab.data)!="Classes"],file="lab.data.predictions.txt")
+
+# You are now ready to predict the field data!
+# You may then open the script "Predicting the field data.R"
